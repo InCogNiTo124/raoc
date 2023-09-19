a6d82607f22619d5ee4a349ce924204e5a436fa3
InCogNiTo124 <msmetko@msmetko.xyz>
2023-09-19 21:22:00 +0200

    2021-01-2

---
+++ 2021/src/bin/01/2.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    let _ = io::stdin().read_to_string(&mut buffer);
+
+    let count = buffer
+        .trim()
+        .split('\n')
+        .map(|x| str::parse::<i32>(x).unwrap())
+        .collect::<Vec<i32>>()
+        .windows(3)
+        .map(|a| a.get(0).unwrap() + a.get(1).unwrap() + a.get(2).unwrap())
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .filter(|a| a.get(1) > a.get(0))
+        .count();
+    println!("{}", count);
+}
