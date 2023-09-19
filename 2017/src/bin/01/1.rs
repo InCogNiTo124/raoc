9643530f94c41dcf00efc1b9de3a6e32fe1237cf
InCogNiTo124 <msmetko@msmetko.xyz>
2023-09-19 21:22:00 +0200

    2017-01-1

---
+++ 2017/src/bin/01/1.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    let _ = io::stdin().read_line(&mut buffer).unwrap();
+    let mut sum = 0;
+    let length = buffer.len()-1;  // avoiding newline
+    for i in 0..length {
+        let letter = buffer.chars().nth(i).unwrap();
+        let next_letter = buffer.chars().nth((i+1)%length).unwrap();
+        if letter == next_letter {
+            sum += letter.to_digit(10).unwrap();
+        }
+    }
+    println!("{}", sum);
+}
