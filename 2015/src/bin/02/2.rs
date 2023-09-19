be317c1852e5692ddfbeed913ddfbb9f9507c122
InCogNiTo124 <msmetko@msmetko.xyz>
2023-09-19 21:22:00 +0200

    2015-02-2

---
+++ 2015/src/bin/02/2.rs
@@ -0,0 +1,28 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    let mut stdin = io::stdin();
+    stdin.read_to_string(&mut buffer).expect("Failed to read line");
+    let result: u32 = buffer
+        .trim()
+        .split("\n")
+        .map(|line| {
+            line.split("x")
+                .map(|number| number.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(|t| {
+            let a = t[0];
+            let b = t[1];
+            let c = t[2];
+            
+            let bow = a*b*c;
+            let wrap = vec![a+b, b+c, a+c].iter().min().unwrap()*2; 
+
+            bow+wrap 
+            })
+        .sum();
+    println!("{}", result);
+}
+
